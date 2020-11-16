0e1cfecf01cd7f23c292b2605b542fb0dfea36f9
yuriipetrenko <obzora6@gmail.com>
2020-11-16 21:13:45 +0100

    added slick slider and accordion

--- assets/js/app.js
+++ assets/js/app.js
@@ -52,4 +52,25 @@ $(function(){
                   $("#nav").toggleClass("active");
          });
 
+
+         /* Collapse */
+         
+         $("[data-collapse]").on("click", function(event) {
+            event.preventDefault();
+    
+            var $this = $(this),
+                blockId = $this.data('collapse');
+    
+            $this.toggleClass("active");
+        });
+
+        /* Slider */
+        
+        $("[data-slider]").slick({
+            infinite: true,
+            fade: false,
+            slidesToShow: 1,
+            slidesToScroll: 11
+        });
+
 });
